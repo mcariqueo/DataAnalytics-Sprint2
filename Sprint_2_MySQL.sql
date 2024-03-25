719f3e729301ddd7ede2415f0586a126eff8a1ab
Marcial Cariqueo A <marcial.cariqueo@gmail.com>
2024-03-25 17:20:23 +0100

    rectificaciones

--- Sprint_2_MySQL.sql
+++ Sprint_2_MySQL.sql
@@ -3,34 +3,35 @@
 ## Exercici 1
 /* Mostra totes les transaccions realitzades per empreses d'Alemanya.*/
 
-	SELECT 	*										
-	FROM 	transactions.company					 
-	JOIN 	transactions.transaction				 
-	ON		transaction.company_id = company.id		 
-	WHERE	company.country = "germany";			
+	
+	SELECT 	*	
+    FROM 	transaction 
+    WHERE 	company_id 	IN 
+			(SELECT	id	
+             FROM	company
+             WHERE	company.country = "germany");			
 
 ## Exercici 2
-/* Màrqueting està preparant alguns informes de tancaments de gestió, et demanen que els passis un llistat de les empreses que han realitzat transaccions per una suma superior a la mitjana de totes les transaccions.*/
+/* Màrqueting està preparant alguns informes de tancaments de gestió,
+ et demanen que els passis un llistat de les empreses que han realitzat
+ transaccions per una suma superior a la mitjana de totes les transaccions.*/
 
 	# 1. Obtener el valor de la mediana de todas las transacciones
     
 	SELECT AVG(amount) FROM transactions.transaction;
-    
-	/* Este valor que obtengo, me servira para usarlo en un where o un having*/
-
-	/* 2. Obtener una tabla que indique las empresas con nombre de pais 
-    que han hecho transacciones mayores que la media*/
-    
-	SELECT 	DISTINCT company.company_name
-	FROM 	transactions.company					# 
-	JOIN 	transactions.transaction				# 
-	ON		transaction.company_id = company.id		# 
-	WHERE	transaction.amount > (SELECT AVG(amount) FROM transactions.transaction)
-    ORDER BY company.company_name;
+        
+	
+	SELECT 	DISTINCT company_name
+	FROM 			 company			
+	WHERE id IN (SELECT	company_id	
+				 FROM transaction 
+                 WHERE amount > (SELECT AVG(amount) FROM transaction));
 
 
 # Exercici 3
-/*El departament de comptabilitat va perdre la informació de les transaccions realitzades per una empresa, però no recorden el seu nom, només recorden que el seu nom iniciava amb la lletra c. Com els pots ajudar? Comenta-ho acompanyant-ho de la informació de les transaccions.*/
+/*El departament de comptabilitat va perdre la informació de les transaccions realitzades per una empresa, 
+però no recorden el seu nom, només recorden que el seu nom iniciava amb la lletra c. 
+Com els pots ajudar? Comenta-ho acompanyant-ho de la informació de les transaccions.*/
 
 		SELECT *
         FROM 	transactions.company					# 
@@ -38,40 +39,19 @@
 	    ON		transaction.company_id = company.id		# 
         WHERE   company.company_name LIKE "c%"; 
 
+		SELECT *			FROM 	transaction
+        WHERE	company_id 	IN 
+							(SELECT id 
+							 FROM 	company 
+							 WHERE  	company_name LIKE "c%"); 
+
+        
+	
+
 # Exercici 4
 
 /* Van eliminar del sistema les empreses que no tenen transaccions registrades,
  lliura el llistat d'aquestes empreses*/
-  
-
-
-#  Hago una comparacion manual del numero de compañyas en cada tabla:  
-		SELECT  		id  		FROM company;	
-   		SELECT 			company_id	FROM transaction;
-        SELECT DISTINCT company_id	FROM transaction;
-
-## Opcion 1:
-/* para ellos deberia entonces tener un tipo de join en el que tenga las empreseas que no tienen transacciones*/
-
-/* Puedo usar un left join y buscar valores que sean nulo. Es decir, con un left join tendré una tabla con aquellas compañias que estando en la tabla company, en la tabla transacition no estan y por lo mismo, en este join el valor será null*/ 
-
- 		SELECT 		company.id, 
-					transaction.amount
-		FROM 		transactions.company						
-		LEFT JOIN	transactions.transaction		 
-		ON			transaction.company_id = company.id		
-        WHERE 		transactions.company.id IS NULL;
-
-/* tambien pudria utilizar una subquery para realizar la misma consulta*/ 
-
-		SELECT	company_id, transaction.amount
-		FROM	transaction
-		WHERE	company_id NOT IN (SELECT id FROM company);
-
-/* el IN sirve para specify multiple possible values for a column, esto lo realiza a traves de una lista de valores , tipo tupla.
-puedo generar una subquery con los valores de una tabla, que en este caso es lo que hago i*/
-
-
 
 # Sprint 2 - Nivel 2
 
@@ -79,54 +59,56 @@ puedo generar una subquery con los valores de una tabla, que en este caso es lo 
 /* Per a això, et demanen la llista de totes les transaccions realitzades per empreses
  que estan situades en el mateix país que aquesta companyia*/
 
-SELECT transaction.* FROM transaction 
-JOIN company on company.id = transaction.company_id
-WHERE company.country = 
-	(SELECT distinct company.country FROM transaction 
-    JOIN company on company.id = transaction.company_id
-    WHERE company_name = "Nunc interdum incorporated");
+SELECT	*	
+FROM 	transaction
+WHERE 	company_id IN (
+		SELECT id FROM company WHERE country = (SELECT country FROM company WHERE company_name = 'Non Institute'));
 
 # Exercici 2
 /* El departament de comptabilitat necessita que trobis l'empresa que ha realitzat 
 la transacció de major suma en la base de dades.*/
 
-SELECT company.company_name FROM transaction 
-join   company on company.id = transaction.company_id
-WHERE amount = (SELECT max(amount) FROM transaction);
+SELECT	c.company_name,	t.amount	
+FROM	company c
+JOIN	transaction t	
+ON		t.company_id = c.id 
+WHERE	t.amount = (SELECT max(amount) FROM transaction);
+
+SELECT 
+    company_id,
+    (SELECT company_name FROM company WHERE id = t.company_id) 	AS nombre,
+    (SELECT max(amount) FROM transaction) 						AS monto
+FROM	transaction t
+WHERE	amount = (SELECT max(amount) FROM transaction);
+
+
+
 
 # Sprint 2 - Nivell 3
 ## Exercici 1
 /* necessiten el llistat dels països la mitjana de transaccions dels quals sigui superior a la mitjana general.*/
 
-SELECT DISTINCT company.country FROM transaction
-JOIN company on company.id = transaction.company_id
-WHERE amount > (SELECT AVG (amount) FROM transaction)
+
+SELECT DISTINCT country from company
+WHERE id IN (
+			SELECT DISTINCT company_id 
+            FROM	transaction 
+            WHERE	amount > (SELECT AVG (amount) FROM transaction))
 ORDER BY 1;
+
+
 
 ## Exercici 2
 /* Vol un llistat de les empreses on especifiquis si tenen més de 4 transaccions o menys.*/
 
-SELECT 		company.company_name,
-    CASE	WHEN COUNT(transaction.id) < 4	THEN "Menys de 4 transacciones"
-			ELSE "Més de 4 transaccions"	END AS result
-FROM		transaction
-JOIN		company ON company.id = transaction.company_id
-GROUP BY	company.company_name;
+SELECT	company_id,
+		(SELECT company_name FROM company c WHERE id IN (company_id)) AS nombre,
+		CASE 	WHEN num_transactions < 4 		THEN 'Menys de 4 transacciones'
+				ELSE 'Més de 4 transaccions' 	END AS result
+FROM	(SELECT t.company_id , 
+				COUNT(t.id) AS num_transactions  
+				FROM transaction t  GROUP BY  t.company_id) AS transaction_counts;
 
 
-
-
-SELECT 
-    company.company_name,
-    CASE 
-        WHEN COUNT(transaction.id) > 4 THEN 'Más de 4 transacciones' 
-        ELSE 'Menos de 4 transacciones' 
-    END AS cantidad_transacciones
-FROM 
-    transaction
-JOIN 
-    company ON company.id = transaction.company_id
-GROUP BY 
-    company.company_name;
 
 
--- Sprint_2_respuestas.md
+++ Sprint_2_respuestas.md
@@ -2,35 +2,22 @@
 # Sprint 2: Nivel 1
 
 ## Exercici 1
-Muestra todas las trasacciones realizadas por empresas de alemania
+Mostra totes les transaccions realitzades per empreses d'Alemanya.	
 
+#### Respuesta
 Para resolver este exercici utilizo el siguiente código: 
-
-![](files_sprin2/S2N1E1-1.png)
-
-
-Utilizo la función WHERE = "germany" para seleccionar solo las empresas de Alemania.
+- Para realizarlo at traves de subquery:
 
 ![logo](files_sprin2/S2N1E1.png)
 
-
-
 ## Exercici 2
+Màrqueting està preparant alguns informes de tancaments de gestió, et demanen que els passis un llistat de les empreses que han realitzat transaccions per una suma superior a la mitjana de totes les transaccions.
 
-Marketing solicita un listado de empresas que hayan realizado transacciones por encima del promedio de todas las transacciones para informes de cierre de gestión.
+#### Respuesta
 
-**Paso 1**: Obtener el valor de la media de todas las transacciones
+**Paso 1**: Genero la subquery para obtener la lista de id de las empresas cuyo valor de la media tranacciones fue mayor a la media.
 
-![](files_sprin2/S2N1E2-1.png)
-
-
-**Paso 2**: Obtener una tabla que indique las empresas que han hecho transacciones mayores que la media
-
-![](files_sprin2/S2N1E2-2.png)
-
-Utilizo las funciones AVG() y DISTINCT para calcular la media de las transacciones y obtener una lista única de países respectivamente.
-
-Finalmente el output total es el siguiente:
+**Paso 2**: Utilizo subquery con el comando ***WHERE*** y con la funcion IN para ligarla a la subquery.
 
 ![](files_sprin2/S2N1E2.png)
 
@@ -39,13 +26,9 @@ Finalmente el output total es el siguiente:
 
 El departament de comptabilitat va perdre la informació de les transaccions realitzades per una empresa, però no recorden el seu nom, només recorden que el seu nom iniciava amb la lletra c. Com els pots ajudar? Comenta-ho acompanyant-ho de la informació de les transaccions.
 
-1. Lo primero es identificar aquellas compañias que cumplen con la condicion de comenzar con la letra "c". 		
-   
-	En este caso bastará con un JOIN y un WHERE con el comando LIKE.
+#### Respuesta
 
-![](files_sprin2/S2N1E3-1.png)
-
-- El comando _**LIKE**_ permite buscar un patrón, que en este caso es que la primera letra comience por "c".
+1. Primero genero la subquery para identificar aquellas compañias que cumplen con la condicion de comenzar con la letra "c". El comando _**LIKE**_ permite buscar un patrón, que en este caso es que la primera letra comience por "c".
 
 El resultado completo es el siguiente: 
 
@@ -56,58 +39,58 @@ El resultado completo es el siguiente:
 
 Van eliminar del sistema les empreses que no tenen transaccions registrades, lliura el llistat d'aquestes empreses.
 
-### Opcion 1
-
-Para ello, deberia tener un tipo de join en el que tenga las emprseas que no tienen transacciones. Puedo usar un left join y buscar valores que sean nulo. 
-
-Es decir, con un left join tendré una tabla con aquellas compañias que estando en la tabla company, en la tabla transacition no estan y por lo mismo, en este join el valor será null
-
-![](files_sprin2/S2N1E4.png)
-### Opcion 2
-Tambien podría utilizar una subquery para realizar la misma consulta
+#### Respuesta
 
 - El _IN_ sirve para specify multiple possible values for a column, 
 - Puedo generar una subquery con los valores de una tabla
 
-En otras palabras, esta parte de la consulta selecciona solo aquellas filas de la tabla _**transaction**_ cuyo _company_id_ no está presente en la columna _id_ de la tabla _**company**_.
-![](files_sprin2/S2N1E4-1.png)
+En otras palabras, esta parte de la consulta selecciona solo aquellas filas de la tabla ***transaction*** cuyo _company_id_ no está presente en la columna _id_ de la tabla ***company***.
 
+![](files_sprin2/S2N1E4.png)
 
-En ambos casos el resultado es null o vacio.
+En este caso todas las compañias tienen transacciones, por lo que el output queda igual a ***company***.
 
-
-
-
-
+///////////
 # Sprint 2- Nivel 2
 ## Exercici 1
+En la teva empresa, es planteja un nou projecte per a llançar algunes campanyes publicitàries per a fer competència a la companyia **Non Institute**. Per a això, et demanen la llista de totes les transaccions realitzades per empreses que estan situades en el mateix país que aquesta companyia.
 
+### Comentario: Nos habian dicho en clase que la empresa era la otra.
+
+### Respuesta Corregida con subquery:
 
 ![](files_sprin2/S2N2E1.png)
 
-- En este caso, para seleccionar las columnas de transaction, podemos utilizar el comando ```transaction.*```
-- Con la subquery tenemos como resultado el pais de la empresa indicada en el enunciado. 
+- Creo una subquery que tenga como resultado el pais de la empresa
+- Despues creo otra query que genere la lista de las empresas que tienen el mismo pais
+- Finalmente genero el Query con las subquery en el **WHERE**
 
 ## Exercici 2
 
+### Respuesta Corregida con subquery:
 ![](files_sprin2/S2N2E2.png)
+
+1. El company_id me serivrá para obtener el nombre y el monto ya que los utilizaré en las otras subqueries del las **SELECT**
+2. Una subquery me que entregue el company_name en base el company_id anterior
+3. Otra subquery que me entreque el monto maximo  de las transacciones.
+4. la siguiente subquery es la importante ya que me permitira tener el company_id necesario para las otras querys
 
 
 # Sprint 2 - Nivell 3
 ## Exercici 1
 S'estan establint els objectius de l'empresa per al següent trimestre, per la qual cosa necessiten una base sòlida per a avaluar el rendiment i mesurar l'èxit en els diferents mercats. Per a això, necessiten el llistat dels països la mitjana de transaccions dels quals sigui superior a la mitjana general.
 
+### Respuesta Corregida con subquery:
+
 ![](files_sprin2/S2N3E1.png)
-
-
-
 
 ## Exercici 2
 El Departament de recursos humans vol un llistat de les empreses on especifiquis si tenen més de 4 transaccions o menys.
 
-Que debo tener?
+### Respuesta Corregida con subquery
 
 ![](files_sprin2/S2N3E2.png)
 
 - En este caso tengo una tabla en la que creo una columna llamada "result" en la que indico si tiene "Mes de 4 trasnacciones" o "menys de 4 trasnacciones"
-- Para ello, utilizo un comando CASE que permite establecer una condicion. 
+- Para ello, utilizo un comando CASE que permite establecer una condicion.
+- En este casodevuelve 101 resultado por que está incluido el b-9999.  
